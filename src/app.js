b05e682e62b6db85654b7f6c8ffed2140a920d9b
Chris <stac1221@gmail.com>
2021-01-06 02:04:52 -0600

    Heroku

--- src/app.js
+++ src/app.js
@@ -24,12 +24,12 @@ const Rounds = db.Rounds
 // %%%%%%%%%%%%%%%%%%%%%%%
 // GLOBAL VARIABLES
 // test user CJ2 = 'U01HD50K9HB'
+// test channel CJ-CJ-2 = 'D01HG7L8V4J'
 const alex = 'U1FA8UTV2'
 const cj = 'U1ESXHU6S'
 const john = 'U6AFFTWTH'
 const line = '————————————————'
-// const channel = 'G6C3FD3V5' // Alex-CJ-John DM
-const channel = 'D01HG7L8V4J' // CJ-CJ2
+const channel = 'G6C3FD3V5' // Alex-CJ-John DM
 let slackTime_hm
 let slackTime_s
 let winners = 0
@@ -231,11 +231,10 @@ ${totalScores}`
 slackEvents.on('message', async (e) => {
   try {
     console.log('Slack EVENT')
-    console.log('channel:', e.channel)
     if (e.text === ':1023:' || e.text === ':1023: ') {
       formatSlackTime(e.ts)
 
-      if (slackTime_hm === '10:23' || slackTime_hm === '12:47') {
+      if (slackTime_hm === '10:23') {
         console.log(`slackTime: ${slackTime_hm}:${slackTime_s}`)
         if (e.user === alex) {
           updateUserPoints('Alex')
